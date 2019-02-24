3721fa3a49b30370fb19e100a480b9af1bd6e685
LukeBillo <n0627514@ntu.ac.uk>
2019-02-24 23:54:17 +0000

    Completed first part, now need to make it continue 'inductively'

--- 3MillionMen_SerialBased/ThreeMillionMenSerial.cpp
+++ 3MillionMen_SerialBased/ThreeMillionMenSerial.cpp
@@ -1,34 +1,99 @@
 #include "ThreeMillionMenSerial.h"
+#include <algorithm>
 
 three_million_men_serial::three_million_men_serial(std::shared_ptr<name_map>& names)
 {
 	_names_by_key = names;
-	_results = std::make_unique<name_list>();
+	_results = std::make_unique<result_list>();
 }
 
 void three_million_men_serial::run()
 {
-	auto names_by_value = std::make_unique<name_map>();
-	auto matched_pairs = std::make_unique<name_uo_map>();
+	auto names_by_key_copy = std::make_shared<name_vector>();
 
 	for (auto i = _names_by_key->begin(); i != _names_by_key->end(); ++i)
 	{
-		names_by_value->operator[](i->second) = i->first;
+		names_by_key_copy->push_back({ i->first, i->second });
 	}
 
+	auto names_by_value = std::make_shared<name_vector>(*names_by_key_copy.get());
+	std::sort(names_by_value->begin(), names_by_value->end(), [](const std::pair<std::string, std::string> &x, const std::pair<std::string, std::string> &y)
+	{
+		return x.second < y.second;
+	});
+
+	auto matched_pairs = std::make_shared<name_vector>();
+	auto unmatched_pairs = std::make_shared<name_vector>();
+
+	const auto name_by_key_it = names_by_key_copy->begin();
+	const auto name_by_value_it = names_by_value->begin();
+
+	auto result = match_pairs_recursive(name_by_key_it, name_by_value_it, names_by_key_copy, names_by_value, matched_pairs, unmatched_pairs);
+
+	auto last_name = is_last(result.non_matches->front()) ? result.non_matches->front() : result.non_matches->back();
+	int size = _names_by_key->size();
+
+	_results->push_back({ size, last_name.second });
+	_results->push_back({ size - 1, last_name.first });
+}
+
+three_million_men_serial::pass_result& three_million_men_serial::match_pairs_recursive(name_vector::iterator name_by_key_it,
+	name_vector::iterator name_by_value_it,
+	std::shared_ptr<name_vector>& names_by_key,
+	std::shared_ptr<name_vector>& names_by_value,
+	std::shared_ptr<name_vector>& matches,
+	std::shared_ptr<name_vector>& non_matches)
+{
+	if (name_by_key_it == names_by_key->end())
+	{
+		const auto last_unmatched_name_it = names_by_value->begin();
+		non_matches->push_back({ last_unmatched_name_it->first, last_unmatched_name_it->second });
+
+		const auto result = new pass_result(matches, non_matches);
+		return *result;
+	}
+
+	if (name_by_value_it == names_by_value->end())
+	{
+		non_matches->push_back({ name_by_key_it->first, name_by_key_it->second });
+		const auto next_name_by_key = names_by_key->erase(name_by_key_it);
+		const auto reset_name_by_value_it = names_by_value->begin();
+
+		return match_pairs_recursive(next_name_by_key, reset_name_by_value_it, names_by_key, names_by_value, matches, non_matches);
+;	}
+
+	if (name_by_key_it->second == name_by_value_it->first)
+	{
+		matches->push_back({ name_by_key_it->first, name_by_value_it->second });
+
+		const auto next_name_by_key = names_by_key->erase(name_by_key_it);
+
+		names_by_value->erase(name_by_value_it);
+		const auto reset_name_by_value_it = names_by_value->begin();
+
+		return match_pairs_recursive(next_name_by_key, reset_name_by_value_it, names_by_key, names_by_value, matches, non_matches);
+;	} 
+	else
+	{
+		const auto next_name_by_value = ++name_by_value_it;
+		return match_pairs_recursive(name_by_key_it, next_name_by_value, names_by_key, names_by_value, matches, non_matches);
+	}
+}
+
+bool three_million_men_serial::is_last(const std::pair<std::string, std::string> unmatched_pair) const
+{
 	for (auto i = _names_by_key->begin(); i != _names_by_key->end(); ++i)
 	{
-		for (auto j = names_by_value->begin(); j != names_by_value->end(); ++j)
+		if (unmatched_pair.second == i->first)
 		{
-			if (i->second == j->first)
-			{
-				matched_pairs->insert({i->first, j->second});
-				_names_by_key->erase(i);
-			}	
+			return false;
 		}
 	}
+
+	return true;
 }
 
 std::list<std::string>* three_million_men_serial::get_results()
 {
+	return new std::list<std::string>();
 }
--- 3MillionMen_SerialBased/ThreeMillionMenSerial.h
+++ 3MillionMen_SerialBased/ThreeMillionMenSerial.h
@@ -8,8 +8,9 @@ class three_million_men_serial
 {
 public:
 	using name_list = std::list<std::string>;
+	using result_list = std::list<std::pair<int, std::string>>;
 	using name_map = std::map<std::string, std::string>;
-	using name_uo_map = std::unordered_map<std::string, std::string>;
+	using name_vector = std::vector<std::pair<std::string, std::string>>;
 
 	three_million_men_serial(std::shared_ptr<name_map>&);
 	~three_million_men_serial() = default;
@@ -19,6 +20,23 @@ public:
 
 private:
 	std::shared_ptr<name_map> _names_by_key;
-	std::unique_ptr<name_list> _results;
+	std::unique_ptr<result_list> _results;
+
+	struct pass_result
+	{
+		std::shared_ptr<name_vector> matches;
+		std::shared_ptr<name_vector> non_matches;
+
+		pass_result(std::shared_ptr<name_vector>& matches, std::shared_ptr<name_vector>& non_matches) : matches(matches), non_matches(non_matches) {}
+	};
+
+	pass_result& match_pairs_recursive(name_vector::iterator name_by_key_it,
+		name_vector::iterator name_by_value_it,
+		std::shared_ptr<name_vector>& names_by_key,
+		std::shared_ptr<name_vector>& names_by_value,
+		std::shared_ptr<name_vector>& matches,
+		std::shared_ptr<name_vector>& non_matches);
+
+	bool is_last(std::pair<std::string, std::string> unmatched_pair) const;
 };
 
